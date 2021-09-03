58ffb194452ebaab5a48eedea888917b9396ff26
Laszlo Pusok <laszlo.pusok@bitrise.io>
2021-09-03 09:48:41 +0200

    List profiles

--- spaceship/spaceship/profile.rb
+++ spaceship/spaceship/profile.rb
@@ -4,6 +4,19 @@ class Cert
     attr_accessor :id
 end
 
+def find_profile(profile_name)
+    platform = "ios"
+    profiles = ProfileClient.fetch_profiles(false, platform)
+
+    for 
+    base64_pem = Base64.encode64(downloaded_portal_cert.to_pem)
+
+    cert_info = {
+      content: base64_pem,
+      id: cert.id
+    }
+end
+
 def create_profile(bundle_id, certificate_id, profile_name)
     cert = Cert.new
     cert.id = certificate_id
