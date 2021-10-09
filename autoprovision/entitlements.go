c4876e7d3a15bfa248dc27e064668930059ed9a9
Bart Whiteley <bwhiteley@ancestry.com>
2021-10-09 10:29:09 -0600

    add ParentApplicationIdentifiers to list of capabilities not present on AppstoreConnect API

--- autoprovision/entitlements.go
+++ autoprovision/entitlements.go
@@ -106,7 +106,7 @@ func (e Entitlement) AppearsOnDeveloperPortal() bool {
 	entKey := serialized.Object(e).Keys()[0]
 
 	capType, ok := appstoreconnect.ServiceTypeByKey[entKey]
-	return ok && capType != appstoreconnect.Ignored && capType != appstoreconnect.ProfileAttachedEntitlement
+	return ok && capType != appstoreconnect.Ignored && capType != appstoreconnect.ProfileAttachedEntitlement && capType != appstoreconnect.ParentApplicationIdentifiers
 }
 
 // Equal ...
